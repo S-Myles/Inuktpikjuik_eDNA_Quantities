fc292985790e21000ad033ec788aa883f960c6ca
S-Myles <shannon.myles@dal.ca>
2025-02-03 09:09:13 -0500

    Added quantitative barplots

--- 03-taxa-barplots.R
+++ 03-taxa-barplots.R
@@ -20,13 +20,34 @@ S12_physeq_agg <- S12_taxfilt_data %>%
 
 # Melt the data for ggplot
 ps_melted <- psmelt(S12_physeq_agg) %>% 
-  filter(Depth..m. < 2)
+  filter(Depth..m. < 2) %>% 
+  mutate(Location = factor(Location, levels = c("Cooler blank 1",
+                                                "Cooler blank 2",
+                                                "Sackville River", 
+                                                "DRDC barge", 
+                                                "McKay bridge", 
+                                                "Tufts Cove",
+                                                "McNabs Island")),
+         Date = format(Date, "%Y-%b-%d"),
+         Date = factor(Date, levels = c("2023-Jun-22",
+                                        "2023-Jul-25",
+                                        "2023-Aug-31",
+                                        "2023-Sep-25",
+                                        "2023-Nov-03",
+                                        "2023-Nov-24",
+                                        "2023-Dec-14",
+                                        "2024-Jan-19",
+                                        "2024-Feb-16",
+                                        "2024-Mar-28",
+                                        "2024-Apr-18",
+                                        "2024-May-16"
+                                        )))
 
 # Create the faceted bar plot with custom facet titles
-(plot <- ggplot(ps_melted, aes(x = Date, y = Abundance, fill = Species)) +
+(plot <- ggplot(ps_melted, aes(x = Location, y = Abundance, fill = Species)) +
     geom_bar(stat = "identity", position = "stack") +
-    facet_grid(~ Location) +
-    labs(x = "Samples", y = "Relative Abundance") +
+    facet_grid(~ Date) +
+    labs(x = "Sampling Location", y = "Relative Abundance") +
     theme_minimal() +
     theme(
       axis.text.x = element_text(angle = 45, hjust = 1),
@@ -38,9 +59,45 @@ ps_melted <- psmelt(S12_physeq_agg) %>%
 ggsave(
   filename = "outputs/metabarcoding_barplots/S12-sp_barplot.png",
   plot = plot,
-  width = 8, height = 6,
+  width = 14, height = 6,
   dpi = 300
 )
+
+
+
+## Make it quantitative w qPCR data
+# Melt the data for ggplot
+ps_melted <- psmelt(S12_physeq_agg) %>% 
+  mutate(quantitative_abundance = Abundance * X12S.copies.mL.SW) %>% 
+  filter(Depth..m. < 2) %>% 
+  mutate(Location = factor(Location, levels = c("Cooler blank 1",
+                                                "Cooler blank 2",
+                                                "Sackville River", 
+                                                "DRDC barge", 
+                                                "McKay bridge", 
+                                                "Tufts Cove",
+                                                "McNabs Island")))
+
+# Create the faceted bar plot with custom facet titles
+(plot <- ggplot(ps_melted, aes(x = Location, y = quantitative_abundance, fill = Species)) +
+    geom_bar(stat = "identity", position = "stack") +
+    facet_grid(~ Date) +
+    labs(x = "Sampling Location", y = "Quantitative Abundance") +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      strip.text = element_text(size = 10),
+      legend.position = "none"
+    ))
+
+# Save the plot
+ggsave(
+  filename = "outputs/metabarcoding_barplots/S12-Quantitative_sp_barplot.png",
+  plot = plot,
+  width = 14, height = 6,
+  dpi = 300
+)
+
 
 
 
@@ -57,13 +114,36 @@ S16_physeq_agg <- S16_taxfilt_data %>%
 
 # Melt the data for ggplot
 ps_melted <- psmelt(S16_physeq_agg) %>% 
-  filter(Depth..m. < 2)
+  mutate(quantitative_abundance = Abundance * X16S.copies.mL.SW) %>% ## Make it quantitative w qPCR data
+  filter(Depth..m. < 2) %>% 
+  mutate(Location = factor(Location, levels = c("Cooler blank 1",
+                                                "Cooler blank 2",
+                                                "Sackville River", 
+                                                "DRDC barge", 
+                                                "McKay bridge", 
+                                                "Tufts Cove",
+                                                "McNabs Island")),
+         Date = format(Date, "%Y-%b-%d"),
+         Date = factor(Date, levels = c("2023-Jun-22",
+                                        "2023-Jul-25",
+                                        "2023-Aug-31",
+                                        "2023-Sep-25",
+                                        "2023-Nov-03",
+                                        "2023-Nov-24",
+                                        "2023-Dec-14",
+                                        "2024-Jan-19",
+                                        "2024-Feb-16",
+                                        "2024-Mar-28",
+                                        "2024-Apr-18",
+                                        "2024-May-16"
+         )))
+
 
 # Create the faceted bar plot with custom facet titles
-(plot <- ggplot(ps_melted, aes(x = Date, y = Abundance, fill = Phylum)) +
+(plot <- ggplot(ps_melted, aes(x = Location, y = Abundance, fill = Phylum)) +
     geom_bar(stat = "identity", position = "stack") +
-    facet_grid( ~ Location) +  # Add the custom labeller here
-    labs(x = "Samples", y = "Relative Abundance") +
+    facet_grid( ~ Date) +  # Add the custom labeller here
+    labs(x = "Sampling Location", y = "Relative Abundance") +
     theme_minimal() +
     theme(
       axis.text.x = element_text(angle = 45, hjust = 1),
@@ -75,9 +155,35 @@ ps_melted <- psmelt(S16_physeq_agg) %>%
 ggsave(
   filename = "outputs/metabarcoding_barplots/S16-phylum_barplot.png",
   plot = plot,
-  width = 8, height = 6,
+  width = 14, height = 6,
   dpi = 300
 )
+
+
+
+# Create the faceted bar plot with custom facet titles
+(plot <- ggplot(ps_melted, aes(x = Location, y = quantitative_abundance, fill = Phylum)) +
+    geom_bar(stat = "identity", position = "stack") +
+    facet_grid(~ Date) +
+    labs(x = "Sampling Location", y = "Quantitative Abundance") +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      strip.text = element_text(size = 10),
+      legend.position = "none"
+    ))
+
+# Save the plot
+ggsave(
+  filename = "outputs/metabarcoding_barplots/S16-Quantitative_sp_barplot.png",
+  plot = plot,
+  width = 14, height = 6,
+  dpi = 300
+)
+
+
+
+
 
 
 
@@ -93,13 +199,35 @@ S18_physeq_agg <- S18_taxfilt_data %>%
 
 # Melt the data for ggplot
 ps_melted <- psmelt(S18_physeq_agg) %>% 
-  filter(Depth..m. < 2)
+  mutate(quantitative_abundance = Abundance * X18S.copies.mL.SW) %>% ## Make it quantitative w qPCR data
+  filter(Depth..m. < 2) %>% 
+  mutate(Location = factor(Location, levels = c("Cooler blank 1",
+                                                "Cooler blank 2",
+                                                "Sackville River", 
+                                                "DRDC barge", 
+                                                "McKay bridge", 
+                                                "Tufts Cove",
+                                                "McNabs Island")),
+         Date = format(Date, "%Y-%b-%d"),
+         Date = factor(Date, levels = c("2023-Jun-22",
+                                        "2023-Jul-25",
+                                        "2023-Aug-31",
+                                        "2023-Sep-25",
+                                        "2023-Nov-03",
+                                        "2023-Nov-24",
+                                        "2023-Dec-14",
+                                        "2024-Jan-19",
+                                        "2024-Feb-16",
+                                        "2024-Mar-28",
+                                        "2024-Apr-18",
+                                        "2024-May-16"
+         )))
 
 # Create the faceted bar plot with custom facet titles
-(plot <- ggplot(ps_melted, aes(x = Date, y = Abundance, fill = Phylum)) +
+(plot <- ggplot(ps_melted, aes(x = Location, y = Abundance, fill = Phylum)) +
     geom_bar(stat = "identity", position = "stack") +
-    facet_grid( ~ Location) +  # Add the custom labeller here
-    labs(x = "Samples", y = "Relative Abundance") +
+    facet_grid( ~ Date) +  # Add the custom labeller here
+    labs(x = "Sampling Location", y = "Relative Abundance") +
     theme_minimal() +
     theme(
       axis.text.x = element_text(angle = 45, hjust = 1),
@@ -111,8 +239,27 @@ ps_melted <- psmelt(S18_physeq_agg) %>%
 ggsave(
   filename = "outputs/metabarcoding_barplots/S18-phylum_barplot.png",
   plot = plot,
-  width = 8, height = 6,
+  width = 14, height = 6,
   dpi = 300
 )
 
 
+# Create the faceted bar plot with custom facet titles
+(plot <- ggplot(ps_melted, aes(x = Location, y = quantitative_abundance, fill = Phylum)) +
+    geom_bar(stat = "identity", position = "stack") +
+    facet_grid(~ Date) +
+    labs(x = "Sampling Location", y = "Quantitative Abundance") +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      strip.text = element_text(size = 10),
+      legend.position = "none"
+    ))
+
+# Save the plot
+ggsave(
+  filename = "outputs/metabarcoding_barplots/S16-Quantitative_sp_barplot.png",
+  plot = plot,
+  width = 14, height = 6,
+  dpi = 300
+)
